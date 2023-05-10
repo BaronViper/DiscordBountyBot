07d92ca7c7bca8ff2779c574e0bbfb881aee9c51
JeremyS-T <jtanrev@gmail.com>
2023-05-10 16:07:29 +0800

    Add edit mission functionality to bot.py.

--- bot.py
+++ bot.py
@@ -72,20 +72,19 @@ def run_discord_bot():
             colour=discord.Colour.from_rgb(0, 0, 0),
             title="Help Menu"
         )
-        embed.add_field(name="Add mission",
-                        value="`Only available to @Game Master`\n Run /add_mission (preferred) or >add_mission to add a new mission. If running >add_mission, the command takes in five inputs separated by spaces (title, description, reward, difficulty- Very Easy, Easy, Medium, Hard, Very Hard, Expert, faction- Rogue, Imperial, Rebel, Mandalorian).\n\n Example Command: >add_mission 'Destroy Rebel Base' 'A big rebel base on Kuwait' '200' 'Easy' 'Imperial'\n")
-        embed.add_field(name="Add bounty",
-                        value="Run /add_bounty (preferred) or >add_bounty to add a new bounty. If running >add_mission, the command takes in four inputs separated by spaces (title, description, reward.\n\n Example Command: >add_bounty 'Jabba' 'I don't like him. I heard he's chilling in his palace on Tatooine.' '300'\n")
-        embed.add_field(name="Delete mission",
-                        value="`Only available to @Game Master`\n Run /delete_mission (preferred) or >delete_mission to delete a mission. If running >delete_mission, the command takes in two inputs (mission_id, faction- Rogue, Imperial, Rebel, Mandalorian).\n\n Example Command: >delete_mission 2 'Mandalorian'\n")
-        embed.add_field(name="Delete bounty",
-                        value="`Only available to @Game Master`\n Run /delete_bounty (preferred) or >delete_bounty to delete a bounty. If running >delete_bounty, the command takes in one input (bounty_id).\n\n Example Command: >delete_bounty 2\n")
         embed.add_field(name="Show missions",
-                        value="Run /all_missions (preferred) or >all_missions to show available missions for faction specified (Rogue, Imperial, Rebel, Mandalorian).\n\n Example Command: >all_missions Mando\n")
+                        value="Run /all_missions (preferred) or >all_missions to show available missions for faction specified (Rogue, Imperial, Rebel, Mandalorian).\n\nExample Command: >all_missions Mando\n\n")
         embed.add_field(name="Show bounties",
                         value="Run /all_bounties (preferred) or >all_bounties to show available bounties.")
+        embed.add_field(name="Add bounty",
+                        value="Run /add_bounty (preferred) or >add_bounty to add a new bounty. If running >add_mission, the command takes in three inputs separated by spaces (title, description, reward).\n\nExample Command: >add_bounty 'Jabba' 'I don't like him. I heard he's chilling in his palace on Tatooine.' '300'\n\n")
+        embed.add_field(name="Add mission",
+                        value="`Only available to @Game Master`\nRun /add_mission (preferred) or >add_mission to add a new mission. If running >add_mission, the command takes in five inputs separated by spaces (title, description, reward, difficulty- Very Easy, Easy, Medium, Hard, Very Hard, Expert, faction- Rogue, Imperial, Rebel, Mandalorian).\n\nExample Command: >add_mission 'Destroy Rebel Base' 'A big rebel base on Kuwait' '200' 'Easy' 'Imperial'\n\n")
+        embed.add_field(name="Delete mission",
+                        value="`Only available to @Game Master`\nRun /delete_mission (preferred) or >delete_mission to delete a mission. If running >delete_mission, the command takes in two inputs (mission_id, faction- Rogue, Imperial, Rebel, Mandalorian).\n\nExample Command: >delete_mission 2 'Mandalorian'\n\n")
+        embed.add_field(name="Delete bounty",
+                        value="`Only available to @Game Master`\nRun /delete_bounty (preferred) or >delete_bounty to delete a bounty. If running >delete_bounty, the command takes in one input (bounty_id).\n\nExample Command: >delete_bounty 2\n\n")
         embed.set_footer(text="Built by BaronViper#8694")
-
         await ctx.send(embed=embed)
 
     @bot.hybrid_command(name="add_mission", description='Adds a new mission')
@@ -111,7 +110,7 @@ def run_discord_bot():
     async def add_mission(ctx, title: str, description: str, reward: int, difficulty: app_commands.Choice[str], faction: app_commands.Choice[str]):
         embed = discord.Embed(colour=discord.Colour.from_rgb(0, 0, 0),
                               title=f"Confirm Mission Details",
-                              description=f"**Title:** {' '.join(x.capitalize() for x in title.split())}\n\n **Faction:** {faction.value}\n\n **Difficulty:** {difficulty.value}\n\n **Reward:** <:credits:1099938341467738122>{reward:,}\n\n **Description:** {description}")
+                              description=f"**Title:** {' '.join(x.capitalize() for x in title.split())}\n\n**Faction:** {faction.value}\n\n**Difficulty:** {difficulty.value}\n\n**Reward:** <:credits:1099938341467738122>{reward:,}\n\n**Description:** {description}")
         embed.set_footer(text="✅ to confirm, or ❌ to cancel.")
 
         bot_response = await ctx.send(embed=embed)
@@ -125,7 +124,7 @@ def run_discord_bot():
         try:
             reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
             if str(reaction.emoji) == "✅":
-                if len(session.query(Missions).all()) == 10:
+                if len(session.query(Missions).filter_by(faction=faction.value).all()) == 10:
                     await ctx.send("Error. Too many missions already listed. Maximum of 10 only.")
                 else:
                     new_mission = Missions(
@@ -145,45 +144,121 @@ def run_discord_bot():
         except asyncio.TimeoutError:
             await ctx.send("Confirmation timed out.")
 
+    @bot.hybrid_command(name="edit_mission", description='Edits a mission')
+    @commands.has_role("Game Master")
+    @app_commands.describe(faction='What faction is this mission for?',
+                           m_id="What is the mission's ID?",
+                           mission_field='What mission info to edit?',
+                           mission_value="What value to replace mission info?")
+    @app_commands.choices(faction=[
+        app_commands.Choice(name="Rogue", value="Rogue"),
+        app_commands.Choice(name="Imperial", value="Imperial"),
+        app_commands.Choice(name="Rebel", value="Rebel"),
+        app_commands.Choice(name="Mandalorian", value="Mandalorian"),
+    ])
+    @app_commands.choices(mission_field=[
+        app_commands.Choice(name="Title", value="Title"),
+        app_commands.Choice(name="Description", value="Description"),
+        app_commands.Choice(name="Reward", value="Reward"),
+        app_commands.Choice(name="Difficulty", value="Difficulty"),
+    ])
+    async def edit_mission(ctx, m_id: int, faction: app_commands.Choice[str], mission_field: app_commands.Choice[str], mission_value: str):
+        targeted_mission = None
+        status_pass = True
+
+        targeted_mission = session.query(Missions).filter_by(faction=faction.value, id=m_id).first()
+        if targeted_mission:
+            if mission_field.value == 'Title':
+                mission_value = ' '.join(x.capitalize() for x in mission_value.split())
+                targeted_mission.title = mission_value
+            elif mission_field.value == 'Description':
+                targeted_mission.description = mission_value
+            elif mission_field.value == 'Reward':
+                try:
+                    mission_value = int(mission_value)
+                    targeted_mission.reward = mission_value
+                except:
+                    await ctx.send("Reward value must be an integer.")
+                    status_pass = False
+            elif mission_field.value == 'Difficulty':
+                title_diff = mission_value.title()
+                available_diff = ['Very Easy', 'Easy', 'Medium', 'Hard', 'Very Hard', 'Expert']
+                if title_diff in available_diff:
+                    targeted_mission.difficulty = title_diff
+                else:
+                    await ctx.send(f"Invalid difficulty set. Available options are: {available_diff}.")
+                    status_pass = False
+
+            if status_pass:
+                embed = discord.Embed(colour=discord.Colour.from_rgb(0, 0, 0),
+                                      title=f"Confirm Edited Mission Details",
+                                      description=f"**Title:** {' '.join(x.capitalize() for x in targeted_mission.title.split())}\n\n **Faction:** {faction.value}\n\n **Difficulty:** {targeted_mission.difficulty}\n\n **Reward:** <:credits:1099938341467738122>{targeted_mission.reward:,}\n\n **Description:** {targeted_mission.description}")
+                embed.set_footer(text="✅ to confirm, or ❌ to cancel.")
+
+                bot_response = await ctx.send(embed=embed)
+
+                await bot_response.add_reaction("✅")
+                await bot_response.add_reaction("❌")
+
+                def check(reaction, user):
+                    return user == ctx.author and str(reaction.emoji) in ["✅", "❌"] and reaction.message == bot_response
+
+                try:
+                    reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
+                    if str(reaction.emoji) == "✅":
+                        session.commit()
+                        await ctx.send("Confirmed! Mission edited.")
+
+                    elif str(reaction.emoji) == "❌":
+                        await ctx.send("Canceled request.")
+
+                except asyncio.TimeoutError:
+                    await ctx.send("Confirmation timed out.")
+        else:
+            await ctx.send("Mission not found.")
+
     @bot.hybrid_command(name="add_bounty", description='Adds a new bounty')
     @commands.has_role("Member")
     @app_commands.describe(target="Who does the bounty target?", description="Bounty description or additional notes?",
-                           reward="How much will you pay upon completion?")
+                           reward="How much will you pay upon completion? Minimum 150 credits!")
     async def add_bounty(ctx, target: str, description: str, reward: int):
-        embed = discord.Embed(
-            colour=discord.Colour.from_rgb(0, 0, 0),
-            title="Confirm Bounty Details",
-            description=f"**Target:** {target}\n\n **Reward:** <:credits:1099938341467738122>{reward:,}\n\n **Client:** {ctx.author}\n\n **Description:** {description}"
-        )
-        embed.set_footer(text="✅ to confirm, or ❌ to cancel.")
+        if reward < 150:
+            await ctx.send("Insufficient reward amount set. Canceled request.")
+        else:
+            embed = discord.Embed(
+                colour=discord.Colour.from_rgb(0, 0, 0),
+                title="Confirm Bounty Details",
+                description=f"**Target:** {target}\n\n**Reward:** <:credits:1099938341467738122>{reward:,}\n\n**Client:** {ctx.author}\n\n**Description:** {description}"
+            )
+            embed.set_footer(text="✅ to confirm, or ❌ to cancel.")
 
-        bot_response = await ctx.send(embed=embed)
+            bot_response = await ctx.send(embed=embed)
 
-        await bot_response.add_reaction("✅")
-        await bot_response.add_reaction("❌")
+            await bot_response.add_reaction("✅")
+            await bot_response.add_reaction("❌")
 
-        def check(reaction, user):
-            return user == ctx.author and str(reaction.emoji) in ["✅", "❌"] and reaction.message == bot_response
+            def check(reaction, user):
+                return user == ctx.author and str(reaction.emoji) in ["✅", "❌"] and reaction.message == bot_response
 
-        try:
-            reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
-            if str(reaction.emoji) == "✅":
-                if len(session.query(Bounties).all()) == 10:
-                    await ctx.send("Error. Too many bounties already listed. Maximum of 10 only.")
-                else:
-                    new_bounty = Bounties(
-                        target=target.title(),
-                        description=description,
-                        reward=reward,
-                        client=str(ctx.author)
-                    )
-                    session.add(new_bounty)
-                    session.commit()
-                    await ctx.send("Confirmed! Bounty added.")
-            elif str(reaction.emoji) == "❌":
-                await ctx.send("Canceled request.")
-        except asyncio.TimeoutError:
-            await ctx.send("Confirmation timed out.")
+            try:
+                reaction, user = await bot.wait_for('reaction_add', timeout=60.0, check=check)
+                if str(reaction.emoji) == "✅":
+                    if len(session.query(Bounties).all()) == 10:
+                        await ctx.send("Error. Too many bounties already listed. Maximum of 10 only.")
+                    else:
+                        new_bounty = Bounties(
+                            target=target.title(),
+                            description=description,
+                            reward=reward,
+                            client=str(ctx.author)
+                        )
+                        session.add(new_bounty)
+                        session.commit()
+                        await ctx.send("Confirmed! Bounty added.")
+                elif str(reaction.emoji) == "❌":
+                    await ctx.send("Canceled request.")
+            except asyncio.TimeoutError:
+                await ctx.send("Confirmation timed out.")
 
     @bot.hybrid_command(name="all_missions", description="Shows all available missions for each faction.")
     @app_commands.describe(faction='What faction missions to show?')
@@ -197,7 +272,7 @@ def run_discord_bot():
         missions = session.query(Missions).filter_by(faction=faction.value).all()
         embed_description = ""
         for mission in missions:
-            embed_description += f"\n\n**<:credits:1099938341467738122>{mission.reward:,} - ID: {missions.index(mission) + 1} - {mission.title} - {mission.difficulty}**\n {mission.description}"
+            embed_description += f"\n\n**<:credits:1099938341467738122>{mission.reward:,} - ID: {missions.index(mission) + 1} - {mission.title} - {mission.difficulty}**\n{mission.description}"
         embed = discord.Embed(
             colour=discord.Colour.from_rgb(0, 0, 0),
             title=f"=== {faction.value} Mission Board ===",
@@ -210,7 +285,7 @@ def run_discord_bot():
         bounties = session.query(Bounties).all()
         embed_description = ""
         for bounty in bounties:
-            embed_description += f"\n\n**<:credits:1099938341467738122>{bounty.reward:,} - ID: {bounties.index(bounty) + 1} - {bounty.target}**\n `From {bounty.client}`\n {bounty.description}"
+            embed_description += f"\n\n**<:credits:1099938341467738122>{bounty.reward:,} - ID: {bounties.index(bounty) + 1} - {bounty.target}**\n`From {bounty.client}`\n{bounty.description}"
         embed = discord.Embed(
             colour=discord.Colour.from_rgb(0, 0, 0),
             title="=== Bounty Board ===",
@@ -235,7 +310,7 @@ def run_discord_bot():
             embed = discord.Embed(
                 colour=discord.Colour.from_rgb(0, 0, 0),
                 title=f"=== {faction.value} Mission Info - ID: {m_id} ===",
-                description=f"**<:credits:1099938341467738122>{mission.reward:,} - {mission.title} - {mission.difficulty}**\n {mission.description}"
+                description=f"**<:credits:1099938341467738122>{mission.reward:,} - {mission.title} - {mission.difficulty}**\n{mission.description}"
             )
             await ctx.send(embed=embed)
 
@@ -249,7 +324,7 @@ def run_discord_bot():
             embed = discord.Embed(
                 colour=discord.Colour.from_rgb(0, 0, 0),
                 title=f"=== Bounty Info - ID: {b_id} ===",
-                description=f"**<:credits:1099938341467738122>{bounty.reward:,} - {bounty.target}**\n `From {bounty.client}`\n {bounty.description}"
+                description=f"**<:credits:1099938341467738122>{bounty.reward:,} - {bounty.target}**\n`From {bounty.client}`\n{bounty.description}"
             )
             await ctx.send(embed=embed)
 
@@ -264,7 +339,7 @@ def run_discord_bot():
             embed = discord.Embed(
                 colour=discord.Colour.from_rgb(0, 0, 0),
                 title=f"=== Delete Bounty - ID: {b_id} ===",
-                description=f"**<:credits:1099938341467738122>{bounty.reward:,} - {bounty.target}**\n `From {bounty.client}`\n {bounty.description}"
+                description=f"**<:credits:1099938341467738122>{bounty.reward:,} - {bounty.target}**\n `From {bounty.client}`\n{bounty.description}"
             )
             embed.set_footer(text="✅ to confirm, or ❌ to cancel.")
 
@@ -304,7 +379,7 @@ def run_discord_bot():
             embed = discord.Embed(
                 colour=discord.Colour.from_rgb(0, 0, 0),
                 title=f"=== Delete {faction.value} Mission - ID: {m_id} ===",
-                description=f"**<:credits:1099938341467738122>{mission.reward:,} - {mission.title} - {mission.difficulty}**\n {mission.description}"
+                description=f"**<:credits:1099938341467738122>{mission.reward:,} - {mission.title} - {mission.difficulty}**\n{mission.description}"
             )
             embed.set_footer(text="✅ to confirm, or ❌ to cancel.")
 
